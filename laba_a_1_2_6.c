b177ac922b73c41cf01ec28ec20ee6cffb6b3254
w1xs <ddmalyshev@yandex.ru>
2024-10-06 14:49:43 +0300

    laba ready

--- laba_a_1_2_6.c
+++ laba_a_1_2_6.c
@@ -8,6 +8,28 @@ int main(){
 
     setlocale(0,"");
     printf("Введите длину последовательности: ");
+
+    // flag = 1;
+    // while(flag){
+    //     scanf("%c", &char_from_len);
+    //     number_in_ASCII = (int)char_from_len;
+    //     if(number_in_ASCII == 10){
+    //         flag = 0;
+    //     }
+    //     else{
+    //         if (number_in_ASCII < 48 || number_in_ASCII > 58){
+    //             flag = 0;
+    //             printf("ERROR: Введен не верный символ");
+    //             return 1;                                
+    //         }
+    //     }
+    // }
+    // Считываем все символы, которые ввел пользователь по одному, проверяем каждый из них по ASCII.
+    // Если до Светы докапаются, нужно будет допилить запись подтвержденных символов в строку и в случае, если 
+    // все символы подходят по условию, эта строка должна собираться в инт. Реализацию придумай на месте.
+    // Самый простой способ скорее всего бежать по проверенной строке от конца к началу и прибавлять I символ к 
+    // заранее подготовленному числу, умножая на его разряд. Ну там "123" = 3*1 + 2*10 + 1*100 = 123 (int)
+
     is_posled_len_number = scanf("%d", &posled_len);
     if(is_posled_len_number){
         if(posled_len >= 0){
@@ -21,8 +43,7 @@ int main(){
             second_star = -1;
             count = 0;
             
-            while(count < posled_len && count < 100){
-                printf("%c", posled[count]);
+            while(count <= posled_len && count < 100){
                 if(posled[count] == '*'){
                     if(first_star == -1){
                         first_star = count;
@@ -47,7 +68,7 @@ int main(){
         }   
     }
     else{
-        printf("ERROR: Длина последовательности может быть только целым числом");
+        printf("ERROR: Длина последовательности может быть только числом");
     }
     return 0;
 }
